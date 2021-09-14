065eaf353d68eb9708f477095b6d7e7b21c6419a
Marco57589 <50150327+Marco57589@users.noreply.github.com>
2021-09-14 18:00:50 +0200

    Delete ona_vehicleIndicator directory

--- ona_vehicleIndicator/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-games { 'gta5' }
-
-author 'OneNoobArmy'
-description 'Vehicle indicators by OneNoobArmy.'
-version '1.0.0'
-
-client_script 'client.lua'
-server_script 'server.lua'
